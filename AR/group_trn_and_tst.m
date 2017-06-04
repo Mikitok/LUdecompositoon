0489ffad4a9f89f3570d4fd2f82c3bbf0dd5189d
Mikito <978808462@qq.com>
2017-06-04 16:09:56 +0800

    AR database

--- AR/group_trn_and_tst.m
+++ AR/group_trn_and_tst.m
@@ -1,50 +1,51 @@
-% load ORL.mat;
-% X_trn=cell(40,1);
-% X_tst=cell(360,1);
-% Y_trn=zeros(40,1);
-% Y_tst=zeros(360,1);
+% load AR.mat;
+% X_trn1=cell(100,1);
+% X_tst1=cell(600,1);
+% X_trn2=cell(100,1);
+% X_tst2=cell(600,1);
+% Y_trn=zeros(100,1);
+% Y_tst=zeros(600,1);
 % count1=1;
 % count2=1;
+% count3=1;
 % for i=1:400
-%     if(rem(i,10)==1)
-%         X_trn{count1}=ORL{i};
-%         Y_trn(count1)=ORL_label(i);
+%     if(rem(i,26)==1)
+%         X_trn1{count1}=AR{i};
+%         Y_trn(count1)=AR_label(i);
+%     else if(rem(i,26)==14)
+%         X_trn2{count1}=AR{i};
 %         count1=count1+1;
-%     else
-%         X_tst{count2}=ORL{i};
-%         Y_tst(count2)=ORL_label(i);
-%         count2=count2+1;
+%         else if(rem(i,26)>1 && rem(i,26)<8)
+%               X_tst1{count2}=AR{i};
+%               Y_tst(count2)=AR_label(i);
+%               count2=count2+1;
+%             else if(rem(i,26)>14 && rem(i,26)<21)
+%                 X_tst2{count3}=AR{i};
+%                 count3=count3+1;
+%                 end
+%             end
+%         end
 %     end
 % end
-% save ORL_group.mat X_trn X_tst Y_trn Y_tst;
+% save AR_group.mat X_trn1 X_tst1 X_trn2 X_tst2 Y_trn Y_tst;
 
 load AR.mat;
-X_trn1=cell(100,1);
-X_tst1=cell(600,1);
-X_trn2=cell(100,1);
-X_tst2=cell(600,1);
-Y_trn=zeros(100,1);
-Y_tst=zeros(600,1);
+X1=cell(700,1);
+X2=cell(700,1);
+Y1=zeros(700,1);
+Y2=zeros(700,1);
 count1=1;
 count2=1;
-count3=1;
-for i=1:400
-    if(rem(i,26)==1)
-        X_trn1{count1}=AR{i};
-        Y_trn(count1)=AR_label(i);
-    else if(rem(i,26)==14)
-        X_trn2{count1}=AR{i};
+for i=1:2600
+    if(rem(i,26)>0 && rem(i,26)<8)
+        X1{count1}=AR{i};
+        Y1(count1)=AR_label(i);
         count1=count1+1;
-        else if(rem(i,26)>1 && rem(i,26)<8)
-              X_tst1{count2}=AR{i};
-              Y_tst(count2)=AR_label(i);
-              count2=count2+1;
-            else if(rem(i,26)>14 && rem(i,26)<21)
-                X_tst2{count3}=AR{i};
-                count3=count3+1;
-                end
-            end
+    else if(rem(i,26)>13 && rem(i,26)<21)
+        X2{count2}=AR{i};
+        Y2(count2)=AR_label(i);
+        count2=count2+1;
         end
     end
 end
-save AR_group.mat X_trn1 X_tst1 X_trn2 X_tst2 Y_trn Y_tst;
+save AR_part.mat X1 X2 Y1 Y2;
---
+++ AR/testlu.m
@@ -0,0 +1,80 @@
+clear;
+load AR_part.mat;
+
+for k=1:15
+% for t=0:6
+X_trn1=cell(100,1);
+X_tst1=cell(600,1);
+X_trn2=cell(100,1);
+X_tst2=cell(600,1);
+Y_trn1=zeros(100,1);
+Y_tst1=zeros(600,1);
+Y_trn2=zeros(100,1);
+Y_tst2=zeros(600,1);
+count1=1;
+count2=1;
+for i=1:700
+   if(rem(i,7)==1)
+        X_trn1{count1}=imresize(X1{i},[120,90]);
+        X_trn2{count1}=imresize(X2{i},[120,90]);
+        Y_trn1(count1)=Y1(i);
+        Y_trn2(count1)=Y2(i);
+        count1=count1+1;
+    else
+        X_tst1{count2}=imresize(X1{i},[120,90]);
+        X_tst2{count2}=imresize(X2{i},[120,90]);
+        Y_tst1(count2)=Y1(i);
+        Y_tst2(count2)=Y2(i);
+        count2=count2+1;
+    end
+end
+
+numtrn=length(X_trn1);
+numtst=length(X_tst1);
+
+[X_lu1,Y_lu1]=ludecomposition(X_trn1,Y_trn1);
+[vec1, ~] = tdfda(X_lu1, max(Y_trn1)) ;
+% k=15;
+
+%%%%%%  使用生成的新的训练样本集分类%%%%%%
+for i=1:numtrn*3
+    X_trn1{i}=double(X_lu1{i})*vec1(:,1:k);
+    %X_trn{i}=double(X_trn{i});
+end
+
+%%%%%%  使用原先的训练样本集分类  %%%%%%%
+% for i=1:numtrn
+%     X_trn1{i}=double(X_trn{i})*vec(:,1:k);
+%     %X_trn{i}=double(X_trn{i});
+% end
+for i=1:numtst
+    X_tst1{i}=double(X_tst1{i})*vec1(:,1:k);
+    %X_tst{i}=double(X_tst{i});
+end
+d=discompute(X_trn1,X_tst1);
+out=distclassify(d, Y_lu1);
+ratek1(1,k)=mean(out==Y_tst1);
+
+[X_lu2,Y_lu2]=ludecomposition(X_trn2,Y_trn2);
+[vec2, ~] = tdfda(X_lu2, max(Y_trn2)) ;
+%%%%%%  使用生成的新的训练样本集分类%%%%%%
+for i=1:numtrn*3
+    X_trn2{i}=double(X_lu2{i})*vec2(:,1:k);
+    %X_trn{i}=double(X_trn{i});
+end
+
+%%%%%%  使用原先的训练样本集分类  %%%%%%%
+% for i=1:numtrn
+%     X_trn1{i}=double(X_trn{i})*vec(:,1:k);
+%     %X_trn{i}=double(X_trn{i});
+% end
+for i=1:numtst
+    X_tst2{i}=double(X_tst2{i})*vec2(:,1:k);
+    %X_tst{i}=double(X_tst{i});
+end
+d=discompute(X_trn2,X_tst2);
+out=distclassify(d, Y_lu2);
+
+ratek2(1,k)=mean(out==Y_tst2);
+
+end
